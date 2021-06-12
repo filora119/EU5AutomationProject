e3a5c3c4eda15eb5e72f409f24f73822f07e1cb5
filora119 <halmurat.filora@gmail.com>
2021-06-12 20:22:38 +0200

    EU5 REVIEW WORK1

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -3,6 +3,9 @@ public class Practice1 {
     public static void main(String[] args) {
 
         System.out.println("EU5 IS Liking Git on intelliJ");
+
+        System.out.println("WORK1");
+
     }
 
 
